19a237f70795e2a06be431d2aede63be8188348a
sycamore one <sycamoreone@riseup.net>
2015-01-23 12:00:01 +0000

    Replace tests by tests using the same alphabet as the library does now.
    The tests that test handling of slices of 0s are failing,
    because the library doesn't handle that case properly right now.

--- base58_test.go
+++ base58_test.go
@@ -1,86 +1,46 @@
 package base58
 
 import (
-	"fmt"
-	"math/big"
+	"bytes"
 	"testing"
 )
 
-type testpair struct {
-	decoded int64
+type test struct {
+	decoded []byte
 	encoded string
 }
 
-var pairs = []testpair{
-	{10002343, "Tgmc"},
-	{1000, "if"},
-	{0, ""},
-}
-
-func ExampleEncodeBig() {
-	buf := EncodeBig(nil, big.NewInt(123456))
-	fmt.Printf("%s\n", buf)
-	// Output:
-	// CGy
-}
-
-func ExampleDecodeToBig() {
-	n, err := DecodeToBig([]byte("CGy"))
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-	fmt.Printf("%d\n", n)
-	// Output:
-	// 123456
+// Test cases have been take from the bitcoinj Java implementation of the Bitcoin protocol.
+var testCases = []test{
+	{ []byte("Hello World"), "JxF12TrwUP45BMd" },
+	{ make([]byte, 1), "1" },
+	{ make([]byte, 4), "1111" },
+	{ make([]byte, 7), "1111111" },
+	{ []byte{}, "" },
 }
 
 func TestEncode(t *testing.T) {
-	for _, p := range pairs {
-		var buf []byte = ([]byte)("noise")
-		buf = EncodeBig(buf, big.NewInt(p.decoded))
-		if string(buf) != "noise"+p.encoded {
-			t.Errorf("unexpected result: %q != %q", string(buf), p.encoded)
+	for _, v := range testCases {
+		if got := Encode(v.decoded); got != v.encoded {
+			t.Logf("expected %s but got %s\n", v.encoded, got)
+			t.Fail()
 		}
 	}
 }
 
 func TestDecode(t *testing.T) {
-	for _, data := range pairs {
-		var buf []byte = []byte(data.encoded)
-		n, err := DecodeToBig(buf)
-		if err != nil {
-			t.Errorf("decoding %q failed: %v", data.encoded, err)
-		}
-		if n.Int64() != data.decoded {
-			t.Errorf("unexpected result: %v != %v", n, data.decoded)
+	for _, v := range testCases {
+		if got, err := Decode(v.encoded); !bytes.Equal(got, v.decoded) || err != nil {
+			t.Logf("expected %v but got %v\n", v.decoded, got)
+			t.Fail()
 		}
 	}
 }
 
-func TestDecodeCorrupt(t *testing.T) {
-	type corrupt struct {
-		input  string
-		offset int
-	}
-	examples := []corrupt{
-		{"!!!!", 0},
-		{"x===", 1},
-		{"x0", 1},
-		{"xl", 1},
-		{"xI", 1},
-		{"xO", 1},
-	}
-
-	for _, e := range examples {
-		_, err := DecodeToBig([]byte(e.input))
-		switch err := err.(type) {
-		case CorruptInputError:
-			if int(err) != e.offset {
-				t.Errorf("Corruption in %q at offset %v, want %v", e.input, int(err), e.offset)
-			}
-		default:
-			t.Error("Decoder failed to detect corruption in", e)
-		}
+func TestCorruptInputError(t *testing.T) {
+	_, err := Decode("This isn't valid base58")
+	if err == nil {
+		t.Logf("decoded invalid data without returning non-nil error\n")
+		t.Fail()
 	}
 }
